c1630dd6047cc65287c64eabb01f0d6d3bd388d0
Anusha <anushadharemane@gmail.com>
2026-05-13 20:14:25 +0530

    Add files via upload

---
+++ nextGreater.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class nextGreater {
+    public static int[] nge(int arr[]){
+        Stack<Integer> stack=new Stack<Integer>();
+        int res[]=new int[arr.length];
+        for(int i=arr.length-1;i>=0;i--){
+            int element=arr[i];
+            while(!stack.isEmpty() && stack.peek()<=element){
+                stack.pop();
+            }
+            res[i]=stack.isEmpty()? -1:stack.peek();
+            stack.push(element);
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.println("Enter a array elements:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int res[]=nge(arr);
+        System.out.println(Arrays.toString(res));
+    }
+}
